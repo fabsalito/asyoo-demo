a44b78c63b19bf38e40b1e4b8db91519bdff2c2a
fabsalito <fabricio.salinas@gmail.com>
2015-08-16 10:55:26 -0500

    Colors and typography were defined

--- js/asyoo.general.js
+++ js/asyoo.general.js
@@ -1,76 +1,76 @@
-$(function() {
-	$("#owl-about").owlCarousel({
-		// Most important owl features
-	    items : 1,
-	    itemsCustom : false,
-	    itemsDesktop : [1199,4],
-	    itemsDesktopSmall : [980,3],
-	    itemsTablet: [768,2],
-	    itemsTabletSmall: false,
-	    itemsMobile : [479,1],
-	    singleItem : false,
-	    itemsScaleUp : false,
+// $(function() {
+// 	$("#owl-about").owlCarousel({
+// 		// Most important owl features
+// 	    items : 1,
+// 	    itemsCustom : false,
+// 	    itemsDesktop : [1199,4],
+// 	    itemsDesktopSmall : [980,3],
+// 	    itemsTablet: [768,2],
+// 	    itemsTabletSmall: false,
+// 	    itemsMobile : [479,1],
+// 	    singleItem : false,
+// 	    itemsScaleUp : false,
 	 
-	    //Basic Speeds
-	    slideSpeed : 200,
-	    paginationSpeed : 800,
-	    rewindSpeed : 1000,
+// 	    //Basic Speeds
+// 	    slideSpeed : 200,
+// 	    paginationSpeed : 800,
+// 	    rewindSpeed : 1000,
 	 
-	    //Autoplay
-	    autoPlay : false,
-	    stopOnHover : false,
+// 	    //Autoplay
+// 	    autoPlay : true,
+// 	    stopOnHover : false,
 	 
-	    // Navigation
-	    navigation : false,
-	    navigationText : ["prev","next"],
-	    rewindNav : true,
-	    scrollPerPage : false,
+// 	    // Navigation
+// 	    navigation : false,
+// 	    navigationText : ["prev","next"],
+// 	    rewindNav : true,
+// 	    scrollPerPage : false,
 	 
-	    //Pagination
-	    pagination : true,
-	    paginationNumbers: false,
+// 	    //Pagination
+// 	    pagination : true,
+// 	    paginationNumbers: false,
 	 
-	    // Responsive 
-	    responsive: true,
-	    responsiveRefreshRate : 200,
-	    responsiveBaseWidth: window,
+// 	    // Responsive 
+// 	    responsive: true,
+// 	    responsiveRefreshRate : 200,
+// 	    responsiveBaseWidth: window,
 	 
-	    // CSS Styles
-	    baseClass : "owl-carousel",
-	    theme : "owl-theme",
+// 	    // CSS Styles
+// 	    baseClass : "owl-carousel",
+// 	    theme : "owl-theme",
 	 
-	    //Lazy load
-	    lazyLoad : false,
-	    lazyFollow : true,
-	    lazyEffect : "fade",
+// 	    //Lazy load
+// 	    lazyLoad : false,
+// 	    lazyFollow : true,
+// 	    lazyEffect : "fade",
 	 
-	    //Auto height
-	    autoHeight : false,
+// 	    //Auto height
+// 	    autoHeight : false,
 	 
-	    //JSON 
-	    jsonPath : false, 
-	    jsonSuccess : false,
+// 	    //JSON 
+// 	    jsonPath : false, 
+// 	    jsonSuccess : false,
 	 
-	    //Mouse Events
-	    dragBeforeAnimFinish : true,
-	    mouseDrag : true,
-	    touchDrag : true,
+// 	    //Mouse Events
+// 	    dragBeforeAnimFinish : true,
+// 	    mouseDrag : true,
+// 	    touchDrag : true,
 	 
-	    //Transitions
-	    transitionStyle : false,
+// 	    //Transitions
+// 	    transitionStyle : false,
 	 
-	    // Other
-	    addClassActive : false,
+// 	    // Other
+// 	    addClassActive : false,
 	 
-	    //Callbacks
-	    beforeUpdate : false,
-	    afterUpdate : false,
-	    beforeInit: false, 
-	    afterInit: false, 
-	    beforeMove: false, 
-	    afterMove: false,
-	    afterAction: false,
-	    startDragging : false,
-	    afterLazyLoad : false
-	});
-});
+// 	    //Callbacks
+// 	    beforeUpdate : false,
+// 	    afterUpdate : false,
+// 	    beforeInit: false, 
+// 	    afterInit: false, 
+// 	    beforeMove: false, 
+// 	    afterMove: false,
+// 	    afterAction: false,
+// 	    startDragging : false,
+// 	    afterLazyLoad : false
+// 	});
+// });
